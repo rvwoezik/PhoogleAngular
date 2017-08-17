97568c408e5c32ad890b1e5c078a0e6be118aed3
rvwoezik <rvwoezik@live.nl>
2017-08-17 09:05:29 +0200

    First commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+﻿import { PhooglePage } from './app.po';
+
+describe('abp-zero-template App', function () {
+    let page: PhooglePage;
+
+    beforeEach(() => {
+        page = new PhooglePage();
+    });
+
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        page.getCopyright().then(value => {
+            expect(value).toEqual(new Date().getFullYear() + ' © Phoogle.');
+        });
+    });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+﻿import { browser, element, by } from 'protractor';
+
+export class PhooglePage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getCopyright() {
+    return element(by.css('app-root div.copyright')).getText();
+  }
+}
